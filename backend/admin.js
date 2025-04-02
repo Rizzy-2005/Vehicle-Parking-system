df5036afed8273a6867ec2c600a6f1a51d3d0b6f
Irene-sketch <irenetreessraj@gmail.com>
2025-04-03 01:54:58 +0530

    Made the current update value visiblee in the admin part, madee the updation of the  rate possible. made the connectivity of admin with others. also corrected the small error statement mistake in the attendant. tried to make the date and time nicely with filtering process.

--- backend/admin.js
+++ backend/admin.js
@@ -1,66 +1,45 @@
 const express = require('express');
-const mysql = require('mysql2');
 const cors = require('cors');
-const path = require('path');
-const PORT = 3000;
-const app = express();
+const router = express.Router();
+const db = require("./db");
 
-//middleware
+router.use(express.json())
+router.use(cors());
 
-app.use(express.json())
-app.use(cors());
-app.use(express.static(path.join(__dirname)));
-app.use(express.static(path.join(__dirname, '..')));
-//db connection
-
-const db = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'NewPassword',
-    database:'ez_park',
-})
-
-
-db.connect((err)=>{
-    if (err){
-        console.error('Database Connection Failed: ',err.message);
-    }
-
-    else{
-        console.log("Connected to MYSQL Database");
-    }
-})
-
-
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Parking-revenue.html'),(err)=>{
-        if (err){
-            console.error("Page not found: ",err.message);
-        }
-    });
-})
-
-app.post('/',(req,res)=>{
+router.post('/',(req,res)=>{
     const query = 'select p.user_id,p.vehicle_no,ai.attendant_name as checkin_attendant,ao.attendant_name as checkout_attendant,v.registered_at, v.checkout_at,p.profit from vehicles as v, parking_record as p LEFT JOIN attendant ai ON p.attendant_in = ai.attendant_id LEFT JOIN attendant ao ON p.attendant_out = ao.attendant_id where p.id = v.id;'
     db.query(query,(err,result)=>{
         if (err){
             console.error("Error Encountered: ",err.message);
         }
-
         if (result.length === 0){
             return res.status(500).json({message:"No records found"});
         }
         res.status(200).json(result);
-        
-
     })
-
 })
 
+router.get("/rate",(req,res) => {
+    db.query("SELECT rate_per_hour FROM admin WHERE admin_id = ?",[req.session.admin_id],(err,result) => {
+        if(err)
+        {
+            console.error("Some error occured: ",err);
+            return res.status(407).json({message: "Some error with the database has occured"})
+        }
+        return res.status(200).json(result);
+    });
+});
 
+router.post("/updaterate",(req,res) => {
+    const {new_rate} = req.body;   
+    db.query("UPDATE admin SET rate_per_hour = ? WHERE admin_id = ?",[new_rate,req.session.admin_id],(err,result) => {
+        if(err)
+        {
+            console.error("Some error occured: ",err);
+            return res.status(407).json({message: "Some error with the database has occured"})
+        }
+        return res.status(200).json({message: "Successfully updated the rate/hour"});
+    });
+});
 
-
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+module.exports = router;
--- backend/main.js
+++ backend/main.js
@@ -5,14 +5,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const session = require("express-session");
-/*
-const admin = require("./admin");
 
-app.use("/admin",admin);
-I am commanding this for now since we have not definied 
-the content in each login.js, user.js.. etc then only we can porperly export it and use it 
-otherwise error in listening the server 
-*/
 app.use(session({
   secret: process.env.SESSION_secret, 
   resave: false,  
@@ -27,10 +20,12 @@ app.use(session({
 const login = require("./login");
 const user = require("./user");
 const attendant = require("./attendant");
+const admin = require("./admin");
 
 app.use("/login",login);
 app.use("/user",user);
 app.use("/attendant",attendant);
+app.use("/admin",admin);
 
 app.use(express.static(path.join(__dirname,"../frontend")));
 
